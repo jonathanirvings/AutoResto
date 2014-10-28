cffb115c8c8fa38487dd01a090cb035367c65ae4
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2014-10-28 20:13:45 +0800

    Added index.php :)))))

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>AutoResto</title>
+</head>
+<body>
+	<marquee behavior="scroll" direction="left" scrollamount="5" style="font-size:100">AutoResto</marquee>
+	<marquee behavior="scroll" direction="left" scrollamount="10" style="font-size:100">AutoResto</marquee>
+	<marquee behavior="scroll" direction="left" scrollamount="20" style="font-size:100">AutoResto</marquee>
+	<marquee behavior="scroll" direction="left" scrollamount="10" style="font-size:100">AutoResto</marquee>
+	<marquee behavior="scroll" direction="left" scrollamount="5" style="font-size:100">AutoResto</marquee>
+</body>
+</html>
