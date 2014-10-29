14cd8934cc8de65560935816b950171dc03119db
unlimitedsky <v.sharon.lynn@gmail.com>
2014-10-30 04:00:52 +0800

    Started booking_new page
    A page where user can see the details of the restaurant, key in the number
    of pax they want to book, and submit the booking.

---
+++ booking_new.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+    <?php
+        ini_set("memory_limit",-1);
+        include "constant.php";
+        include "dbhandler.php";
+        include "dboperation.php";
+        include "booking.php";
+        include "restaurant.php";
+        include "customer.php";
+        include "eventhandler.php";
+    ?>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:700italic,400,300,700' rel='stylesheet' type='text/css'>
+    <!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/skel.min.js"></script>
+    <script src="js/skel-panels.min.js"></script>
+    <script src="js/init.js"></script>
+    <noscript>
+        <link rel="stylesheet" href="css/skel-noscript.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/style-desktop.css" />
+    </noscript>
+</head>
+
+<body class="homepage">
+    <?php
+
+    ?>
+    <!-- Header -->
+    <div id="header">
+        <div class="container">
+                
+            <!-- Logo -->
+            <div id="logo">
+                <h1><a href="#">Ex Machina</a></h1>
+            </div>
+            
+            <!-- Nav -->
+            <nav id="nav">
+                <ul>
+                   <li class="active"><a href="index.html">Homepage</a></li>
+                    <li><a href="left-sidebar.html">Left Sidebar</a></li>
+                    <li><a href="right-sidebar.html">Right Sidebar</a></li>
+                    <li><a href="no-sidebar.html">No Sidebar</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <!-- Header -->
+
+    <!-- Banner -->
+        <div id="banner">
+            <div class="container">
+            </div>
+        </div>
+    <!-- /Banner -->
+
+     <!-- Main -->
+        <div id="page">
+            <div id="main" class="container">
+                <div class="row">
+                    <div class="15u">
+                        <section>
+                            <header>
+                                <h2>Restaurant Name</h2>
+                            </header>
+                            <a href="#" class="button">Book</a>
+                        </section>
+                    </div>
+                </div>
+            </div>
+    <!-- Main -->
+
+    </div>
+</body>
+</html>
