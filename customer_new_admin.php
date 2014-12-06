1534509aefd19b1795e07286e604c72c3daf0036
Steven Kester Yuwono <yulonglong2005@yahoo.com>
2014-12-06 14:07:30 +0800

    Added Create New Admin Page
    Page is only available to admin user

--- customer_new_admin.php
+++ customer_new_admin.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <?php
-    include "htmlHeader.php";
+    include "htmlHeaderLogin.php";
     ?>
     <script>
         $(document).ready(function() {
@@ -13,25 +13,30 @@
 </head>
 
 <body class="homepage">
-    <?php
+   <?php
         include "headerPage.php";
+        
+        global $eventHandler;
+        global $customer_is_admin;
+
+        $ic_number = $_SESSION["ic_number"];
+        $customer_details = $eventHandler->getCustomerDetails($ic_number);
+
+        if($customer_details[$customer_is_admin] == 0){
+            echo "<script>
+            window.location.href = \"no_permission.php\";
+            </script>";
+        }
+        
     ?>
     <script>$(".manage_account").addClass("active")</script>
     <?php
-    
-        global $eventHandler;
-        
-        $ic_number = $_SESSION["ic_number"];
-        $customer_details = $eventHandler->getCustomerDetails($ic_number);
-        
-        global $customer_name;
-        global $customer_email;
-        global $customer_ic_no;
-        global $customer_contact_no;
-        
-        $name = $customer_details[$customer_name];
-        $email = $customer_details[$customer_email];
-        $contact_no = $customer_details[$customer_contact_no];
+        $ic_number = "";
+        $name = "";
+        $email = "";
+        $contact_no = "";
+        $password = "";
+        $password_confirm = "";
         
         function checkContactNo($contact_no) {
             for ($i = 0; $i < strlen($contact_no); ++$i) {
@@ -45,20 +50,40 @@
             return true;
         }
     
+        function checkPassword($password,$password_confirm) {
+            if ($password != $password_confirm) {
+                ?><script>
+                    alert("Password does not match");
+                </script><?php
+                return false;
+            }
+            return true;
+        }
         
-        function editUser() {
+        function addUser() {
             global $eventHandler;
             
-            $ic_number = $_SESSION["ic_number"];
+            $ic_number = $_POST["ic_number"];
             $name = $_POST["name"];
             $email = $_POST["email"];
             $contact_no = $_POST["contact_no"];
-            
+            $password = $_POST["password"];
+            $password_confirm = $_POST["password_confirm"];
+            if (!checkPassword($password,$password_confirm)) {
+                return false;
+            }
+            if (!checkContactNo($contact_no)) {
+                return false;
+            }
             $newArray = array();
+            $newArray["ic_no"] = $ic_number;
             $newArray["name"] = $name;
             $newArray["email"] = $email;
             $newArray["contact_no"] = $contact_no;
-            $error_message = $eventHandler->editCustomer($ic_number,$newArray);
+            $newArray["password"] = $password;
+            $newArray["active"] = 1;
+            $newArray["isAdmin"] = 1;
+            $error_message = $eventHandler->addCustomer($newArray);
             ?><script>
             alert("<?php echo $error_message; ?>");
             window.location.href = "index.php";
@@ -67,10 +92,9 @@
         }
     
         if (isset($_POST["hidden_add"])) {
-            editUser();
+            addUser();
         }
     ?>
-    
 
      <!-- Main -->
         <div id="page">
@@ -79,7 +103,7 @@
                     <div class="15u">
                         <section>
                             <header>
-                                <span class="byline">Update Particular</span>
+                                <span class="byline">Admin Sign Up</span>
                             </header>
                                 
                             <div id="bookingoptions" class="container">
@@ -87,30 +111,20 @@
                                     <input type="hidden" name="hidden_add" value=true>
                                     
                                     <h3>IC Number</h3>
-                                    <h3><?php echo $ic_number;?></h3>
+                                    <h3><input type="text" id="ic_number" name="ic_number" size="20" value="<?php echo $ic_number;?>"/></h3>
                                     <h3>Name</h3>
                                     <h3><input type="text" id="name" name="name" size="20" value="<?php echo $name;?>"/></h3>
                                     <h3>Email</h3>
                                     <h3><input type="text" id="email" name="email" size="20" value="<?php echo $email;?>"/></h3>
                                     <h3>Contact Number</h3>
                                     <h3><input type="text" id="contact_no" name="contact_no" size="20" value="<?php echo $contact_no;?>"/></h3>
-                                    <input type="submit" name="save" id="save" class="button" value="Update"/>
+                                    <h3>Password</h3>
+                                    <h3><input type="password" id="password" name="password" size="20" value="<?php echo $password;?>"/></h3>
+                                    <h3>Confirm Password</h3>
+                                    <h3><input type="password" id="password_confirm" name="password_confirm" size="20" value="<?php echo $password_confirm;?>"/></h3>
+                                    <input type="submit" name="save" id="save" class="button" value="Sign Up"/>
                                 </form>
                             </div>
-                            <br>
-                            <a href="change_password.php">Change Password?</a>
-                            <br>
-                            <?php
-                                global $eventHandler;
-                                global $customer_is_admin;
-        
-                                $ic_number = $_SESSION["ic_number"];
-                                $customer_details = $eventHandler->getCustomerDetails($ic_number);
-                                
-                                if($customer_details[$customer_is_admin] == 1){
-                                    echo "<a href=\"customer_new_admin.php\">Create new admin account?</a>";
-                                }
-                            ?>
                         </section>
                     </div>
                 </div>
